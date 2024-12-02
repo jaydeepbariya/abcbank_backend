2db684699700bd78a82fe109d1ef7596695a92c6
jaydeepbariya <jaydeepbariya59@gmail.com>
2024-12-02 14:56:35 +0530

    logging, exception handling - config done. setup complete.

--- src/main/java/com/abcbank/AbcBankApplication.java
+++ src/main/java/com/abcbank/AbcBankApplication.java
@@ -1,13 +1,25 @@
 package com.abcbank;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AbcBankApplication {
+public class AbcBankApplication implements Runnable{
+
+	Logger logger = LogManager.getLogger(AbcBankApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbcBankApplication.class, args);
 	}
 
-}
+	@Override
+	public void run() {
+			logger.info("Info level log example");
+			logger.debug("Debug level log example");
+			logger.error("Error level log example", new Exception("Example exception"));
+		}
+	}
+
